060ec176019597276522ffbe703babb93de5db4e
Juraj Jarmek <badjjj@live.com>
2019-11-09 12:17:31 +0000

    deleting folders

--- Griffith/Griffith - Group Project/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.faq_question h3').click(function(){
-		$(this).next('.faq_content').slideToggle();
-		$(this).parent().toggleClass('active');
-		$(this).parent().siblings().children('.faq_content').slideUp();
-		$(this).parent().siblings().removeClass('active');
-	});
-});
